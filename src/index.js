084234fff3099f05e63c1e840cba075ae007cda5
Jana Ezzat <janaezzat362@gmail.com>
2026-03-19 08:55:14 +0200

    Remove unused useState import
    Removed unused import of useState from React.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
-import { useState } from "react";
 
 // import StarRating from "./StarRating";
 // import './Challenge';
